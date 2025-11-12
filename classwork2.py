1736c8036a70d15400abaa453e442e89a8324ad1
chukwu onwuka <chuksonweb@yahoo.com>
2025-11-12 16:09:35 +0100

    made changes on classwork2

--- classwork2.py
+++ classwork2.py
@@ -1,1 +1,68 @@
 # List Methods
+# Append 
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("orange")
+print(fruits)
+
+
+#Python List clear() Method
+fruits = ['apple', 'banana', 'cherry', 'orange']
+fruits.clear()
+print(fruits)
+
+#Python List copy() Method
+fruits = ['apple', 'banana', 'cherry', 'orange']
+
+x = fruits.copy()
+print(x)
+
+
+#Python List count() Method
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.count("cherry")
+print(x)
+
+#Python List extend() Method
+fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+points = (1, 4, 5, 9)
+fruits.extend(points)
+print(fruits)
+
+#Python List index() Method
+fruits = [4, 55, 64, 32, 16, 32]
+x = fruits.index(32)
+print(x)
+
+#Python List insert() Metho
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1, "orange")
+print(fruits)
+
+#Python List pop() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.pop(1)
+print(fruits)
+
+#Python List remove() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove("banana")
+print(fruits)
+
+#Python List reverse() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.reverse()
+print(fruits)
+
+#Python List sort() Method
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort()
+print(cars)
+
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort(reverse=True)
+print(cars)
